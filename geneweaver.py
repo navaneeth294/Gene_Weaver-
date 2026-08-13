3aa0b0512d2af155872e7b7ce1c57c063c09b2a4
Navaneeth <navaneethkr494@gmail.com>
2026-08-13 14:52:07 +0530

    dna sequence check

--- geneweaver.py
+++ geneweaver.py
@@ -1,20 +1,8 @@
-import numpy as np
-from numba import cuda
-
-@cuda.jit
-def add_kernel(x, y, out):
-    i = cuda.grid(1)
-    if i < x.size:
-        out[i] = x[i] + y[i]
-
-n = 1000
-x = np.arange(n, dtype=np.float32)
-y = np.arange(n, dtype=np.float32)
-out = np.zeros(n, dtype=np.float32)
-
-threads_per_block = 128
-blocks = (n + threads_per_block - 1) // threads_per_block
-add_kernel[blocks, threads_per_block](x, y, out)
-
-print("First 5 results:", out[:5])
-print("Success!" if np.allclose(out, x + y) else "Mismatch!")
+def count_mismatches(seq1, seq2):
+    mismatches = 0
+    for sequence1,sequence2 in zip(seq1,seq2):
+        
+        if sequence1 != sequence2:
+            mismatches += 1
+    return mismatches
+print(count_mismatches("ACGTAC","ATGTGC"))
