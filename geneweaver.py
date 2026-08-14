9763b0de7093b9e80acaba736967415a7ab43a82
Navaneeth <navaneethkr494@gmail.com>
2026-08-14 17:57:44 +0530

    max_mismatches

--- geneweaver.py
+++ geneweaver.py
@@ -1,3 +1,4 @@
+# FUNCTION TO COUNT THE MISMATCHES IN A SEQUENCE
 def count_mismatches(seq1, seq2):
     mismatches = 0
     for sequence1,sequence2 in zip(seq1,seq2):
@@ -5,4 +6,20 @@ def count_mismatches(seq1, seq2):
         if sequence1 != sequence2:
             mismatches += 1
     return mismatches
-print(count_mismatches("ACGTAC","ATGTGC"))
+#print(count_mismatches("ACGTAC","ATGTGC"))
+
+
+#function to find max_matcheble pair
+def find_matches(genome,guide,max_mismatches):
+    n = len(genome)
+    k= len(guide)
+    for i in range(n-k+1):
+        matches = [i:i+1]
+        mismatches = count_mismatches(matches,guide)
+        if mismatches <= max_mismatches
+        return matches
+    return None
+       
+genome = "ACGTACGTTTGCATGCAG"
+guide =  "GTTTG"
+print(find_matches(genome, guide, max_mismatches=1))
