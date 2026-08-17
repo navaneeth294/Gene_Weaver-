a5f046a7d8e59c4961d67f3bcd50965aa75c4652
Navaneeth <navaneethkr494@gmail.com>
2026-08-17 11:42:13 +0530

    encoder

---
+++ encode_seq.py
@@ -0,0 +1,9 @@
+import numpy as np
+def encode_seq(seq):
+    base_to_num = {"A":0, "C":1, "G":2, "T":3}
+    result = []
+    for letter in seq:
+        result.append(base_to_num[letter])
+    return np.array(result, dtype=np.uint8)
+
+print(encode_seq("ACCT"))
