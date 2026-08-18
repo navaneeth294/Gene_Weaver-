1375d71e67915e6bf6a19d08ce9408724161ff88
Navaneeth <navaneethkr494@gmail.com>
2026-08-19 01:16:25 +0530

    encode of genome

--- encode_seq.py
+++ encode_seq.py
@@ -1,4 +1,5 @@
 import numpy as np
+import mock_fasta as mf
 def encode_seq(seq):
     base_to_num = {"A":0, "C":1, "G":2, "T":3}
     result = []
@@ -6,4 +7,4 @@ def encode_seq(seq):
         result.append(base_to_num[letter])
     return np.array(result, dtype=np.uint8)
 
-print(encode_seq("ACCT"))
+print(encode_seq(mf.mock_fasta.fasta))
