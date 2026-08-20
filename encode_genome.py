d8706a7c56fdd801a6a363195f496e6f8fd7b175
Navaneeth <navaneethkr494@gmail.com>
2026-08-20 22:59:29 +0530

    output array

--- encode_genome.py
+++ encode_genome.py
@@ -1,5 +1,6 @@
 from Bio import SeqIO
 from encode_seq import encode_seq  
+import numpy as np
 
 record = next(SeqIO.parse("mock_genome.fasta", "fasta"))
 genome = str(record.seq)
@@ -9,3 +10,9 @@ enc_guide = encode_seq("GTTTG")
 
 print(len(enc_genome))
 print(len(enc_guide))
+
+n = len(enc_genome) - len(enc_guide) + 1
+out = np.zeros(n, dtype=np.uint8)
+
+print(n)
+print(out)
